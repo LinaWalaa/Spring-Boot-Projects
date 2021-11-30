9b4f31be8ecd35e191f05ae773f147163f5a7ed9
Lina Walaa <linawalaa95@gmail.com>
2021-11-30 22:28:06 +0200

    Create a data.sql file and try enabling h2 db link

--- Project 1 - SuperDuperDrive/nd035-c1-spring-boot-basics-project-starter-master/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ Project 1 - SuperDuperDrive/nd035-c1-spring-boot-basics-project-starter-master/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/login", "/signup", "/css/**", "/js/**").permitAll()
+                    .antMatchers("/", "/login", "/signup", "/css/**", "/js/**", "/h2").permitAll()
                     .anyRequest().authenticated()
                     .and()
 
---
+++ Project 1 - SuperDuperDrive/nd035-c1-spring-boot-basics-project-starter-master/starter/cloudstorage/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO USERS(username, salt, password, firstname, lastname) VALUES ('root', 'AAAAAAAAAAAAAAAAAAAAAA==', '6lvEAhfgZ0ElbqPiSX2okQ==', 'Lina', 'Walaa');
+
+INSERT INTO NOTES (userid, notetitle, notedescription) VALUES ('1', 'Saturday','GYM + Study');
+INSERT INTO NOTES (userid, notetitle, notedescription) VALUES ('1', 'Sunday','GYM + Study');
+INSERT INTO NOTES (userid, notetitle, notedescription) VALUES ('1', 'Monday','GYM + Study + Work');
+INSERT INTO NOTES (userid, notetitle, notedescription) VALUES ('1', 'Tuesday','GYM + Study + Work');
+INSERT INTO NOTES (userid, notetitle, notedescription) VALUES ('1', 'Wednesday','GYM + Study + Work');
+INSERT INTO NOTES (userid, notetitle, notedescription) VALUES ('1', 'Thursday','GYM + Study + Work');
+INSERT INTO NOTES (userid, notetitle, notedescription) VALUES ('1', 'Friday','GYM + Study + Work');
+
+INSERT INTO CREDENTIALS (userid, url, username, key, password) VALUES ('1', 'https://www.facebook.com', 'lalash', 'huLxb2mTjLj7ToFvLGLadw==', 'SefZrBi+LBG/Wa2AQK7jiw==');
+INSERT INTO CREDENTIALS (userid, url, username, key, password) VALUES ('1', 'https://www.linkedin.com', 'boda', 'ur/cX6xCZcn0zhl0W+Vqsg==', 'ZQIRsb1s3r4hVvJlnjf5JA==');
+INSERT INTO CREDENTIALS (userid, url, username, key, password) VALUES ('1', 'https://www.youtube.com', 'meshmesh', 'O+/3lnX6Xdc3LQFPt8HCmA==', 'VSgeyUAz+V2wEqE5nwkXnA==');
